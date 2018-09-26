b3444acb8f4136ad5156ed0fade37f58d7960a05
Shelton Gayles <sgbythelake@gmail.com>
2018-09-26 11:37:18 -0700

    created user and blog models

--- pupblog/__init__.py
+++ pupblog/__init__.py
@@ -1,6 +1,31 @@
+import os
 from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
 
 app = Flask(__name__)
+
+################
+## DB SETUP
+###############
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+###################
+## LOGIN CONFIGS
+login_manager = LoginManager()
+
+login_manager.init_app(app)
+login_manager.login_view = 'users.login'
+
+
+###################
 
 from pupblog.core.views import core
 app.register_blueprint(core)
