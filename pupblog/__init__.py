b41e6626c2f0d7f2c1aa66ecbf3796e2be13439e
Shelton Gayles <sgbythelake@gmail.com>
2018-09-26 11:03:47 -0700

    Added 404 and 403 error handling

--- pupblog/__init__.py
+++ pupblog/__init__.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 
 from pupblog.core.views import core
 app.register_blueprint(core)
+
+
+from pupblog.error_pages.handlers import error_pages
+app.register_blueprint(error_pages)
